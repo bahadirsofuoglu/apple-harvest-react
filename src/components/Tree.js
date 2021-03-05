c7a0fd226c5de15863376507bed1ec762f0446b8
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-03-05 22:50:26 +0300

    Tree component added

---
+++ src/components/Tree.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Tree () {
+  return (
+    <div className='tree-zone'>
+      <div className='tree shakeclass'>
+        <div className='head h1' />
+        <div className='head h2' />
+        <div className='head h3' />
+        <div className='head h4' />
+        <div className='head h5' />
+        <div className='head h6' />
+        <div className='trunk' />
+      </div>
+    </div>
+  )
+}
+export default Tree
--- src/views/Index.js
+++ src/views/Index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Apples from '../components/Apples'
 import Basket from '../components/Basket'
-
+import Tree from '../components/Tree'
 function Index () {
   return (
     <div className='container'>
       <div className='tree-zone'>
         <Apples />
+        <Tree />
         <Basket />
       </div>
     </div>
